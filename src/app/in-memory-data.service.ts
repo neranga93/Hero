1401465dfaafe513942a55409f81ae7c4a5bcf3b
yoha93 <nerangayohan@yahoo.com>
2018-05-12 08:01:37 +0530

    pushed the project to git

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,18 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+      { id: 1, name: 'A' },
+      { id: 2, name: 'B' },
+      { id: 3, name: 'C' },
+      { id: 4, name: 'D' },
+      { id: 5, name: 'E' },
+      { id: 6, name: 'F' },
+      { id: 7, name: 'G' },
+      { id: 8, name: 'H' },
+      { id: 9, name: 'I' }
+    ];
+    return {heroes};
+  }
+}
